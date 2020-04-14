03bf1b8ea29f9b48d18f61fca4a959caa5065bc8
Luis.Morales <Luis.Morales@HPG4-DSKTP.fios-router.home>
2020-04-14 15:18:05 -0400

    removed unused classes

--- MySQLTest3/src/application/CarsController.java
+++ MySQLTest3/src/application/CarsController.java
@@ -23,7 +23,6 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.control.TableView;
